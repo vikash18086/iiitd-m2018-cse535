7358cd318106c0f8426edca5bb71208d1fa285eb
vikash kumar pandey <vikascsepandey@gmail.com>
2018-08-15 15:18:36 +0530

    First commit

---
+++ Hello_world/app/src/main/java/com/example/vikas/hello_world/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.vikas.hello_world;
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String msg1="\0",msg2="\0",msg3="\0",msg4="\0",msg5="\0",msg6="\0",msg7="\0";
+
+    private static String state="";
+    private static final String tag ="data";
+    private static final String MSG ="data";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Activity-1 Created",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"Activity-1 is created");
+        state = "Activity-1 On create ";
+
+//        if(savedInstanceState != null){
+//            Log.d("STATE",savedInstanceState.toString());
+//        }
+//        Log.d("CREATION" , "onCreate() is being executed!");
+//        setContentView(R.layout.activity_main);
+        //log created*****************************************************************
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"State of main activity change from "+state+" to On-start",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-1 On-start");
+        state = "Activity-1 On-start";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"State of main activity change from "+state+" to on-Resume",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-1 on-Resume");
+        state = "Activity-1 on-Resume";
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"State of main activity change from "+state+" to on-Pause",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-1 on-Pause");
+        state = "Activity-1 on-Pause";
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"State of main activity change from "+state+" to on-Stop",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-1 on-Stop");
+        state = "Activity-1 on-Stop";
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"State of main activity change from "+state+" to on-Destroy",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-1 on-Destroy");
+        state = "Activity-1 on-Destroy";
+    }
+
+    public void svData(View v) {
+        if ((((EditText) findViewById(R.id.name)).getText().toString().length() < 1)) {
+            Toast.makeText(this, "Name can-not be Empty", Toast.LENGTH_SHORT).show();
+        } else if ((((EditText) findViewById(R.id.rollnumber)).getText().toString().length() < 1)) {
+            Toast.makeText(this, "Roll-no can-not be Empty", Toast.LENGTH_SHORT).show();
+        } else if ((((EditText) findViewById(R.id.branch)).getText().toString().length() < 1)) {
+            Toast.makeText(this, "Branch can-not be Empty", Toast.LENGTH_SHORT).show();
+        }
+        else if ((((EditText) findViewById(R.id.course1)).getText().toString().length() < 1)
+                && (((EditText) findViewById(R.id.course2)).getText().toString().length() < 1)
+                && (((EditText) findViewById(R.id.course3)).getText().toString().length() < 1)
+                && (((EditText) findViewById(R.id.course4)).getText().toString().length() < 1))
+        {
+            Toast.makeText(this, "Atleast one course have to be selected", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            msg1 = ((EditText) findViewById(R.id.name)).getText().toString();
+            msg2 = ((EditText) findViewById(R.id.rollnumber)).getText().toString();
+            msg3 = ((EditText) findViewById(R.id.branch)).getText().toString();
+            msg4 = ((EditText) findViewById(R.id.course1)).getText().toString();
+            msg5 = ((EditText) findViewById(R.id.course2)).getText().toString();
+            msg6 = ((EditText) findViewById(R.id.course3)).getText().toString();
+            msg7 = ((EditText) findViewById(R.id.course4)).getText().toString();
+            String[] records = {msg1, msg2, msg3, msg4, msg5, msg6, msg7};
+            Intent intent = new Intent(getApplicationContext(), Student_Record.class);
+            intent.putExtra(MSG, records);
+            startActivity(intent);
+        }
+    }
+
+    public void clrData(View v1)
+    {
+        ((EditText)findViewById(R.id.name)).getText().clear();
+        ((EditText)findViewById(R.id.rollnumber)).getText().clear();
+        ((EditText)findViewById(R.id.branch)).getText().clear();
+        ((EditText)findViewById(R.id.course1)).getText().clear();
+        ((EditText)findViewById(R.id.course2)).getText().clear();
+        ((EditText)findViewById(R.id.course3)).getText().clear();
+        ((EditText)findViewById(R.id.course4)).getText().clear();
+    }
+}
---
+++ Hello_world/app/src/main/java/com/example/vikas/hello_world/Student_Record.java
@@ -0,0 +1,92 @@
+package com.example.vikas.hello_world;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Student_Record extends AppCompatActivity {
+
+    private static String state="";
+    private static final String tag ="data";
+
+    private static final String MSG ="data";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_record);
+        Intent intent = getIntent();
+        String []message = intent.getStringArrayExtra(MSG);
+
+        Toast.makeText(this,"Activity-2 Created",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"Activity-2 is created");
+        state = "Activity-2 On create ";
+
+
+//      TextView textview0 = (TextView) findViewById(R.id.nameText);
+
+        TextView textview0 = new TextView(this);
+        textview0.setTextSize(20);
+        textview0.setText("Name - "+message[0]+"\n"+"Roll-Number - "+message[1] + "\n"+"Branch - "+message[2] + "\n"
+        +"Course.1 - "+message[3] + "\n" + "Course.2 - "+message[4] + "\n" + "Course.3 - "+message[5] + "\n"
+        +"Course.4 - "+message[6] + "\n");
+        setContentView(textview0);
+
+
+//        TextView textView6 = new TextView(this);
+//        textView6.setTextSize(30);
+//        textView6.setText("Course.3 - "+message[5] + "\n");
+//        setContentView(textView6);
+//
+//        TextView textView7 = new TextView(this);
+//        textView7.setTextSize(30);
+//        textView7.setText("Course.4 - "+message[6] + "\n");
+//        setContentView(textView7);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"State of Activity-2 change from "+state+" to On-start",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-2 On-start");
+        state = "Activity-2 On-start";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"State of Activity-2 change from "+state+" to on-Resume",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-2 on-Resume");
+        state = "Activity-2 on-Resume";
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"State of Activity-2 change from "+state+" to on-Pause",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-2 on-Pause");
+        state = "Activity-2 on-Pause";
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"State of Activity-2 change from "+state+" to on-Stop",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-2 on-Stop");
+        state = "Activity-2 on-Stop";
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"State of Activity-2 change from "+state+" to on-Destroy",Toast.LENGTH_SHORT).show();
+        Log.i(tag,"State change from "+state+" to Activity-2 on-Destroy");
+        state = "Activity-2 on-Destroy";
+    }
+}
